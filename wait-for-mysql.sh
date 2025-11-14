9811747288f3b102f24f3daba87645f72002b52a
VaidikAgnihotri45 <agnihotrivaidik@gmail.com>
2025-11-14 21:23:35 +0530

    Create wait-for-mysql.sh

---
+++ wait-for-mysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+timeout=60
+count=0
+while [ $count -lt $timeout ]; do
+  if mysqladmin ping &>/dev/null; then
+    echo "MySQL is up"
+    break
+  fi
+  count=$((count+1))
+  sleep 1
+done
+/opt/app/mysql-init.sh || true
